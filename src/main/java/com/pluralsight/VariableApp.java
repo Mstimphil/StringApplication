21dccfeb8dcab07a98119d99c00674f765c36155
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-24 13:36:18 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import static java.awt.Color.blue;
+import static java.awt.Color.getColor;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        //Question 1:
+        //declare variables here
+        //then code solution
+        //then use System.out.println() to display results
+        //ex: System.out.println(" The answer is + answer);
+
+        //REPEAT FOR NEXT EXERCISE
+
+   String word= getColor("blue"}
+    int = 2025
+    char = "L"
+
+}
+
